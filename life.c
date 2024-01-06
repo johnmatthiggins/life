4d9ae13ea061048691d45013c510ef061628a089
John Higgins <johnmatthiggins@gmail.com>
2024-01-05 23:33:54 -0800

    got it to compile

--- life.c
+++ life.c
@@ -8,8 +8,8 @@
 
 
 // store two states for the game screen...
-uint8_t screen1[SCREEN_SIZE * SCREEN_SIZE];
-uint8_t screen2[SCREEN_SIZE * SCREEN_SIZE];
+uint8_t screen1[SCREEN_SIZE][SCREEN_SIZE];
+uint8_t screen2[SCREEN_SIZE][SCREEN_SIZE];
 
 int main(int argc, char** argv) {
     memset(screen1, 0, SCREEN_SIZE * SCREEN_SIZE);
@@ -17,8 +17,8 @@ int main(int argc, char** argv) {
 
     create_glider(screen1, 10, 10);
 
-    uint8_t* old = screen1;
-    uint8_t* new = screen2;
+    uint8_t** old = screen1;
+    uint8_t** new = screen2;
 
     for (int i = 0; i < 1000; ++i) {
         sleep(1);
@@ -26,7 +26,7 @@ int main(int argc, char** argv) {
         print_board(new);
 
         // swap boards...
-        uint8_t* tmp = old;
+        uint8_t** tmp = old;
         old = new;
         new = tmp;
     }
@@ -34,7 +34,7 @@ int main(int argc, char** argv) {
     return 0;
 }
 
-void create_glider(uint8_t* board, size_t x, size_t y) {
+void create_glider(uint8_t** board, size_t x, size_t y) {
     board[x][y] = 1;
     board[x + 1][y] = 1;
     board[x - 1][y] = 1;
@@ -42,7 +42,7 @@ void create_glider(uint8_t* board, size_t x, size_t y) {
     board[x][y - 2] = 1;
 }
 
-void print_board(uint8_t* board) {
+void print_board(uint8_t** board) {
     for (size_t i = 0; i < SCREEN_SIZE; ++i) {
         for (size_t j = 0; j < SCREEN_SIZE; ++j) {
             if (board[i][j]) {
@@ -53,7 +53,7 @@ void print_board(uint8_t* board) {
     }
 }
 
-void next_generation(uint8_t* old_board, uint8_t* new_board) {
+void next_generation(uint8_t** old_board, uint8_t** new_board) {
     int adjacent;
     int neighbors;
 
@@ -190,26 +190,26 @@ void next_generation(uint8_t* old_board, uint8_t* new_board) {
     // all squares inside
     for (size_t i = 1; i < SCREEN_SIZE - 1; ++i) {
         for (size_t j = 1; i < SCREEN_SIZE - 1; ++j) {
-            next_cell(old_board, new_board, x, y);
+            next_cell(old_board, new_board, i, j);
         }
     }
 }
 
-void next_cell(uint8_t* old_board, uint8_t* new_board, size_t x, size_t y) {
-    int adjacent = old_board[i - 1][j]
-        | (old_board[i][j - 1] << 1)
-        | (old_board[i - 1][j - 1] << 2)
-        | (old_board[i + 1][j] << 3)
-        | (old_board[i + 1][j + 1] << 4)
-        | (old_board[i][j + 1] << 5)
-        | (old_board[i - 1][j + 1] << 6)
-        | (old_board[i + 1][j - 1] << 7);
+void next_cell(uint8_t** old_board, uint8_t** new_board, size_t x, size_t y) {
+    int adjacent = old_board[x - 1][y]
+        | (old_board[x][y - 1] << 1)
+        | (old_board[x - 1][y - 1] << 2)
+        | (old_board[x + 1][y] << 3)
+        | (old_board[x + 1][y + 1] << 4)
+        | (old_board[x][y + 1] << 5)
+        | (old_board[x - 1][y + 1] << 6)
+        | (old_board[x + 1][y - 1] << 7);
 
     int neighbors = __builtin__popcount(adjacent);
 
-    if (old_board[i][j]) {
-        new_board[i][j] = 1 - (neighbors < 2) - (neighbors > 3);
+    if (old_board[x][y]) {
+        new_board[x][y] = 1 - (neighbors < 2) - (neighbors > 3);
     } else {
-        new_board[i][j] = neighbors == 3;
+        new_board[x][y] = neighbors == 3;
     }
 }
--- life.h
+++ life.h
@@ -6,10 +6,10 @@
 
 #define SCREEN_SIZE (50)
 
-void create_glider(uint8_t* board, size_t x, size_t y);
-void print_board(uint8_t* board);
-void next_generation(uint8_t* old_board, uint8_t* new_board);
-void next_cell(uint8_t* old_board, uint8_t* new_board, size_t x, size_t y);
+void create_glider(uint8_t** board, size_t x, size_t y);
+void print_board(uint8_t** board);
+void next_generation(uint8_t** old_board, uint8_t** new_board);
+void next_cell(uint8_t** old_board, uint8_t** new_board, size_t x, size_t y);
 
 
 #endif
